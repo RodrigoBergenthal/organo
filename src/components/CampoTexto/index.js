8c2e0e73598912264c5741d57502f3d92caf160b
Rodrigo Bergenthal <rodrigo_bergenthal@live.com>
2025-10-29 21:06:38 -0300

    atualizando form

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,11 +1,17 @@
 import './CampoTexto.css';
 const CampoTexto = (props) => {
+
+    let valor = 'Rodrigo';
+    const aoDigitado = (evento) => {
+        valor = evento.target.value;
+        console.log(valor);
+    }
     return(
         <div className="campo-texto">
             <label>
                 {props.label}
                 </label>
-            <input placeholder={props.placeholder} />
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
     );
 
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -13,14 +13,19 @@ const Formulario = () => {
         'Inovação e Gestão'
     ];
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('Formulário submetido');
+    };
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite o seu Cargo" />
-                <CampoTexto label="Imagem" placeholder="Digite o seu endereço da imagem" />
-                <ListaSuspensa label="Time" itens={times} />
+                <CampoTexto obrigatorio={true} label="Nome"  placeholder="Digite o seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu Cargo" />
+                <CampoTexto obrigatorio={true} label="Imagem" placeholder="Digite o seu endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
                 <Botao>
                     Criar Card
                 </Botao>
